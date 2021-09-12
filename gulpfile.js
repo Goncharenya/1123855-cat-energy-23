a9f7762bd14f4a31c9272e5942653d18430bd274
Goncharenya <kotmay@mail.ru>
2021-09-12 23:20:00 +0300

    правки автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,16 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
-
-const htmlmin = require('gulp-html-minifier');
+const htmlmin = require('gulp-htmlmin');
 const csso = require('postcss-csso');
 const rename = require('gulp-rename');
-const jsmin = require('gulp-jsmin');
-// const squoosh = require('gulp-libsquoosh');
+const jsmin = require('gulp-terser');
+const del = require("del");
+const squoosh = require('gulp-libsquoosh');
 const webp = require('gulp-webp');
 const svgStore = require('gulp-svgstore');
+
+
 
 // Styles
 
@@ -22,10 +24,12 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -36,7 +40,7 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -47,11 +51,18 @@ const server = (done) => {
 
 exports.server = server;
 
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
+
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/js/script.js", gulp.series(buildJs));
 }
 
 // build tasks
@@ -62,21 +73,17 @@ const buildHtml = () => {
     .pipe(gulp.dest('build'));
 }
 
-const buildStyles = () => {
-  return gulp.src('source/css/style.css')
-    .pipe(postcss([
-      csso ()
-    ]))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build'));
-}
-
 const buildJs = () => {
   return gulp.src('source/js/*.js')
     .pipe(jsmin())
     .pipe(rename('scripts.min.js'))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build/js'))
+    .pipe(sync.stream());
 }
+
+const clean = () => {
+  return del("build");
+};
 
 const optimizeImages = () => {
   return gulp.src('source/img/**/*.{jpg,png,svg}')
@@ -90,7 +97,7 @@ const copyImages = () => {
 }
 
 const createWebp = () => {
-  return gulp.src('source/img/**/*{img,png}')
+  return gulp.src('source/img/**/*{jpg,png}')
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest('build/img'));
 }
@@ -104,38 +111,64 @@ const createSprite = () => {
     .pipe(gulp.dest('build/img'));
 }
 
-const copyOther = () => {
+const copyOther = (done) => {
   gulp.src([
     "source/fonts/*.{woff,woff2}",
     "source/*.ico",
-    "source/img/*.svg"
+    "source/img/*.svg",
+    "!source/img/icons/*.svg"
   ], {
     base: "source"
   })
-    .pipe(gulp.dest('build/img'));
-  // done();
+    .pipe(gulp.dest('build'));
+   done();
 }
 
-const build = () => {
-  buildHtml();
-  buildStyles();
-  buildJs();
-  copyImages();
-  // optimizeImages();
-  createWebp();
-  createSprite();
-  copyOther();
+// const build = () => {
+//   buildHtml();
+//   styles();
+//   buildJs();
+//   copyImages();
+//   optimizeImages();
+//   createWebp();
+//   createSprite();
+//   copyOther();
+//
+//   return new Promise(function(resolve, reject) {
+//     resolve();
+//   });
+// }
 
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
-
-exports.default = gulp.series(
-  styles, server, watcher
+const build = gulp.series(
+  clean,
+  copyOther,
+  copyImages,
+  gulp.parallel(
+    styles,
+    buildHtml,
+    buildJs,
+    createSprite,
+    createWebp
+  ),
 );
 
 exports.build = build;
 // exports.build = gulp.series(
 //   build
 // );
+
+exports.default = gulp.series(
+  clean,
+  copyOther,
+  copyImages,
+  gulp.parallel(
+    styles,
+    buildHtml,
+    buildJs,
+    createSprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
