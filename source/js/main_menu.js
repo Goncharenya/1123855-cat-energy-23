abde66fa77e25f0ada45b8328acc4654729a3464
Goncharenya <kotmay@mail.ru>
2021-09-18 13:43:40 +0300

    automatic

--- source/js/main_menu.js
+++ source/js/main_menu.js
@@ -1,8 +1,10 @@
-let navList = document.querySelector('.nav__list'),
+let body = document.querySelector('body'),
+  navList = document.querySelector('.nav__list'),
     menuOpen = document.querySelector('.menu__open'),
     navBurgerOpen = document.querySelector('.nav__burger-open'),
     nav = document.querySelector('.nav');
 
+body.classList.remove("no-js");
 navList.classList.remove("menu__open");
 navBurgerOpen.classList.remove("menu__hidden");
 
